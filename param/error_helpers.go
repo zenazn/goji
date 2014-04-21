dce7a18ad6bfd62cc16d714ba46843fb00db34ee
Carl Jackson <carl@avtok.com>
2014-04-21 21:15:09 +0200

    I spel reel gud

--- param/error_helpers.go
+++ param/error_helpers.go
@@ -22,7 +22,7 @@ func perr(format string, a ...interface{}) {
 }
 
 // Problem exists between keyboard and chair. This function is used in cases of
-// programmer error, i.e. an inappripriate use of the param library, to
+// programmer error, i.e. an inappropriate use of the param library, to
 // immediately force the program to halt with a hopefully helpful error message.
 func pebkac(format string, a ...interface{}) {
 	err := errors.New(errPrefix + fmt.Sprintf(format, a...) + yourFault)
