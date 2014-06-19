9a6729e2eab232c62592dde59966c4a238eface0
Carl Jackson <carl@avtok.com>
2014-06-19 00:29:55 -0700

    Don't look up s.literals[i] twice
    This is worth about 3% in
    github.com/julienschmidt/go-http-routing-benchmark's
    BenchmarkGoji_GithubAll. Not a huge win, but definitely still worth it.

--- web/pattern.go
+++ web/pattern.go
@@ -168,10 +168,11 @@ func (s stringPattern) match(r *http.Request, c *C, dryrun bool) bool {
 		matches = make(map[string]string, len(s.pats))
 	}
 	for i := 0; i < len(s.pats); i++ {
-		if !strings.HasPrefix(path, s.literals[i]) {
+		sli := s.literals[i]
+		if !strings.HasPrefix(path, sli) {
 			return false
 		}
-		path = path[len(s.literals[i]):]
+		path = path[len(sli):]
 
 		m := strings.IndexRune(path, '/')
 		if m == -1 {
