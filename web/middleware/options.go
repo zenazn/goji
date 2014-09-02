61acf687f4fae894883fe8dd9e25dcd9c03c3f59
Vincent Vanackere <vincent.vanackere@gmail.com>
2014-09-02 14:50:59 +0200

    Rewrite AutomaticOptions as a NotFound handler. The rewritten tests now pass.

--- web/middleware/options.go
+++ web/middleware/options.go
@@ -8,74 +8,17 @@ import (
 	"github.com/zenazn/goji/web"
 )
 
-type autoOptionsState int
-
-const (
-	aosInit autoOptionsState = iota
-	aosHeaderWritten
-	aosProxying
-)
-
-// I originally used an httptest.ResponseRecorder here, but package httptest
-// adds a flag which I'm not particularly eager to expose. This is essentially a
-// ResponseRecorder that has been specialized for the purpose at hand to avoid
-// the httptest dependency.
-type autoOptionsProxy struct {
-	w     http.ResponseWriter
-	c     context.Context
-	state autoOptionsState
-}
-
-func (p *autoOptionsProxy) Header() http.Header {
-	return p.w.Header()
-}
-
-func (p *autoOptionsProxy) Write(buf []byte) (int, error) {
-	switch p.state {
-	case aosInit:
-		p.state = aosHeaderWritten
-	case aosProxying:
-		return len(buf), nil
-	}
-	return p.w.Write(buf)
-}
-
-func (p *autoOptionsProxy) WriteHeader(code int) {
-	methods := getValidMethods(p.c)
-	switch p.state {
-	case aosInit:
-		if methods != nil && code == http.StatusNotFound {
-			p.state = aosProxying
-			break
-		}
-		p.state = aosHeaderWritten
-		fallthrough
-	default:
-		p.w.WriteHeader(code)
+// AutomaticOptions is a NotFound handler that automatically returns an
+// appropriate "Allow" header when the request method is OPTIONS and the
+// request would have otherwise been 404'd.
+func AutomaticOptions(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	if r.Method != "OPTIONS" {
+		http.NotFound(w, r)
 		return
 	}
-
-	methods = addMethod(methods, "OPTIONS")
-	p.w.Header().Set("Allow", strings.Join(methods, ", "))
-	p.w.WriteHeader(http.StatusOK)
-}
-
-// AutomaticOptions automatically return an appropriate "Allow" header when the
-// request method is OPTIONS and the request would have otherwise been 404'd.
-func AutomaticOptions(h web.Handler) web.Handler {
-	fn := func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-		if r.Method == "OPTIONS" {
-			w = &autoOptionsProxy{c: ctx, w: w}
-		}
-
-		h.ServeHTTPC(ctx, w, r)
-	}
-
-	return web.HandlerFunc(fn)
-}
-
-func getValidMethods(c context.Context) []string {
-	return web.ValidMethods(c)
+	methods := addMethod(web.ValidMethods(ctx), "OPTIONS")
+	w.Header().Set("Allow", strings.Join(methods, ", "))
+	w.WriteHeader(http.StatusOK)
 }
 
 func addMethod(methods []string, method string) []string {
