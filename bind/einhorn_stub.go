3b2ab247aa6cd50b7c3fe966065901fc928ac980
Carl Jackson <carl@avtok.com>
2015-01-06 16:03:21 +0100

    Get rid of many appengine build tags
    This is now controlled at the top level goji package: these packages are
    not even compiled in when using App Engine. This is sensible, since
    neither package is of any use there.

--- bind/einhorn_stub.go
+++ bind/einhorn_stub.go
@@ -1,4 +1,4 @@
-// +build windows appengine
+// +build windows
 
 package bind
 
--- serve_appengine.go
+++ serve_appengine.go
@@ -2,7 +2,16 @@
 
 package goji
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
+
+func init() {
+	if fl := log.Flags(); fl&log.Ltime != 0 {
+		log.SetFlags(fl | log.Lmicroseconds)
+	}
+}
 
 // Serve starts Goji using reasonable defaults.
 func Serve() {
